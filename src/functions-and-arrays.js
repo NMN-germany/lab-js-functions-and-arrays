e2b420eb6286f015c5fed38e9f25df490a9bb243
Noemí Mira <noemi.miranavarro@gmail.com>
2025-04-26 09:55:39 +0200

    Solved lab-functions-and-arrays

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,34 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(num1,num2) {
+        if (num1 >= num2) {
+           return num1;
+        }
+           else {
+             return (num2);
+           }
+    }
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+if(words.length === 0) {
+    return null;
+}
+    let longestWord = "";
+    for (let i = 0; i < words.length; i++) {
+        const currentWord = words[i];
+        if (currentWord.length > longestWord.length){
+            longestWord = currentWord;
+           }
+        else if (currentWord == "") {
+            longestWord = "null"
+        }
+      
+    } return longestWord;
+
+}
 
 
 
@@ -15,15 +36,36 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
-
+function sumNumbers(arrNumbers) {
+    let total = 0;
+    for(let i = 0; i < arrNumbers.length; i++) {
+        const currentNumber = arrNumbers[i];
+        total += currentNumber;
+    }
+      return total;
+}
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function sumNumbers(arrNumbers) {
+    let total = 0;
+    for(let i = 0; i < arrNumbers.length; i++) {
+        const currentNumber = arrNumbers[i];
+        total += currentNumber;
+    }
+      return total;
+}
+
+function averageNumbers(arrNumbers) {
+    if (arrNumbers.length === 0) {
+        return null;
+    }
+    const total = sumNumbers(arrNumbers);
+    const average = total / arrNumbers.length;
+    return average;
+}
+
 
 
 
@@ -31,4 +73,10 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arrWords, wordToFind) {
+    if (arrWords.length === 0) {
+        return null;
+    }
+
+    return arrWords.includes(wordToFind);
+}
